294b87440f2757bc51b3ecd4b9c6f33d337e34d8
VHClarke <VanessaHClarke@gmail.com>
2021-02-19 17:32:56 -0800

    CW III

---
+++ pos_neg_sums.js
@@ -0,0 +1,33 @@
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+If the input array is empty or null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+function countPositivesSumNegatives(input) {
+    return (input && input.length >= 1)
+      ? input.reduce((acc, n) => {
+          n > 0 ? acc[0]++ : acc[1] += n;
+          return acc;
+        }, [0, 0])
+    : [];
+}
+
+//tests
+describe("Example tests", function(){
+    var testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+    var actual = countPositivesSumNegatives(testData);
+    var expected = [10, -65];
+
+    Test.expect(actual[0] == expected[0] && actual[1] == expected[1], "Wrong return value.");
+
+    testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+    actual = countPositivesSumNegatives(testData);
+    expected = [8, -50];
+
+    Test.expect(actual[0] == expected[0] && actual[1] == expected[1], "Wrong return value.");
+});
