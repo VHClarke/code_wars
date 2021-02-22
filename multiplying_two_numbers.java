be7169b26ae112de61fb90e0cd3edd582edc0036
VHClarke <VanessaHClarke@gmail.com>
2021-02-22 11:35:32 -0800

    Daily CW I

---
+++ multiplying_two_numbers.java
@@ -0,0 +1,11 @@
+
+// Implement a function which multiplies two numbers.
+
+public class Kata {
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}
+
+//test
+Test.assertEquals(multiply(2, 3), 6);
